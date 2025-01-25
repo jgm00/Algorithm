f06ae60706617429fd6ce8b3252a03a058c25d6a
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-01-26 03:17:44 +0900

    [Silver III] Title: N과 M (3), Time: 388 ms, Memory: 67852 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -2,23 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] isused;
-	static int[] arr;
 	static int N, M;
+	static int[] selected;
 	static StringBuilder sb = new StringBuilder();
 
-
 	static void dfs(int k) {
-		if(k==M) {
-			for(int i=0;i<M;i++) {
-				sb.append(arr[i] + " ");
+		if (k == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
 			}
-			sb.append('\n');
+			sb.append("\n");
 			return;
 		}
-		for (int i = 1; i < N + 1; i++) {
-			arr[k] = i;
-			dfs(k+1);
+		for (int i = 1; i <= N; i++) {
+			selected[k] = i;
+			dfs(k + 1);
 		}
 	}
 
@@ -27,8 +25,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		isused = new int[N + 1];
-		arr = new int[M];
+		selected = new int[N+1];
 		dfs(0);
 		System.out.println(sb.toString());
 	}
