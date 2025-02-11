f4afb2fc5175524cea7b6d95b9d6b6f524f861c9
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-12 04:06:55 +0900

    [Silver III] Title: 피보나치 함수, Time: 132 ms, Memory: 16100 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int[][] Fibo;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Fibo = new int[41][2];
+		int T = Integer.parseInt(br.readLine());
+		Fibo[0][0] = 1;
+		Fibo[0][1] = 0;
+		Fibo[1][0] = 0;
+		Fibo[1][1] = 1;
+		for (int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			for (int j = 2; j <= k; j++) {
+				Fibo[j][0] = Fibo[j - 1][0] + Fibo[j - 2][0];
+				Fibo[j][1] = Fibo[j - 1][1] + Fibo[j - 2][1];
+			}
+			System.out.println(Fibo[k][0] + " " + Fibo[k][1]);
+		}
+	}
+}
