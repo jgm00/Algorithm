d2d3c4eaa10adf60cc922177bdc1174f2b7cd9b0
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-22 22:00:34 +0900

    [Silver I] Title: 절댓값 힙, Time: 976 ms, Memory: 106200 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,28 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int x;
-	static int N;
+
+	static PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1, o2) -> {
+		if (Math.abs(o1) == Math.abs(o2)) {
+			return o1 < o2 ? -1 : 1;
+		}
+		return Math.abs(o1) < Math.abs(o2) ? -1 : 1;
+	});
 
 	public static void main(String args[]) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
-			if(Math.abs(o1) == Math.abs(o2)) {
-				return o1 < o2 ? -1 : 1;
-			}
-			return Math.abs(o1) < Math.abs(o2) ? -1 : 1;
-		});
 		int N = sc.nextInt();
 		for (int i = 0; i < N; i++) {
 			int k = sc.nextInt();
-			if (k == 0) {
-				if (pq.isEmpty() == false) {
-					System.out.println(pq.poll());
-				} else {
-					System.out.println(0);
-				}
-			} else {
+			if (k != 0) {
 				pq.offer(k);
+			} else {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
 			}
 		}
 	}
