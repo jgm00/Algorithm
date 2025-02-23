abadb35e86245561cbd0e6b9771f8eb205eaeb86
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-23 16:38:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2576 ms, Memory: 325212 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,52 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N;
-	static int[] arr, temp;
+	static int[] nums;
 	static StringBuilder sb = new StringBuilder();
 
-	static void merge(int st, int en) {
-		int mid = (st + en) / 2;
-		int lidx = st;
-		int ridx = mid;
-		for (int i = st; i < en; i++) {
-			if (ridx == en) {
-				temp[i] = arr[lidx++];
-			} else if (mid == lidx) {
-				temp[i] = arr[ridx++];
-			} else if (arr[lidx] <= arr[ridx]) {
-				temp[i] = arr[lidx++];
-			} else {
-				temp[i] = arr[ridx++];
-			}
-		}
-		for (int j = st; j < en; j++) {
-			arr[j] = temp[j];
-		}
-	}
-
-	static void merge_sort(int st, int en) {
-		int mid = (st + en) / 2;
-		if (st + 1 == en) {
-			return;
-		}
-		merge_sort(st, mid);
-		merge_sort(mid, en);
-		merge(st, en);
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		temp = new int[N];
+	public static void main(String args[]) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		nums = new int[N];
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			nums[i] = sc.nextInt();
 		}
-		merge_sort(0, N);
-		for (int num : arr) {
-			sb.append(num+"\n");
+		Arrays.sort(nums);
+		for (int i = 0; i < N; i++) {
+			sb.append(nums[i]).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
