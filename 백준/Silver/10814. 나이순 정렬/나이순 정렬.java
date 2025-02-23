79db73894a17b2b221344e56ce11dac1be138c15
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-23 18:13:48 +0900

    [Silver V] Title: 나이순 정렬, Time: 1220 ms, Memory: 56024 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,40 +1,42 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	static int N;
 
-	static class Pair {
-		int k;
-		String s;
+	static class Student implements Comparable<Student> {
 		int idx;
+		int age;
+		String name;
 
-		Pair(int k, String s, int idx) {
-			this.k = k;
-			this.s = s;
+		Student(int idx, int age, String name) {
 			this.idx = idx;
+			this.age = age;
+			this.name = name;
+		}
+
+		@Override
+		public int compareTo(Student o1) {
+			if (this.age == o1.age) {
+				return this.idx - o1.idx;
+			}
+			return this.age - o1.age; // 오름차순
 		}
 	}
+
+	static Student[] students;
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		Pair[] pairs = new Pair[N];
+		students = new Student[N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			String b = st.nextToken();
-			pairs[i] = new Pair(a, b, i);
+			students[i] = new Student(i, Integer.parseInt(st.nextToken()), st.nextToken());
 		}
-		Arrays.sort(pairs, (o1, o2) -> {
-			if(o1.k == o2.k) {
-				return o1.idx - o2.idx;
-			} 
-			return o1.k - o2.k;
-		});
-		for(int i=0;i<N;i++) {
-			System.out.println(pairs[i].k + " " + pairs[i].s);
+		Arrays.sort(students);
+		for (int i = 0; i < N; i++) {
+			System.out.println(students[i].age + " " + students[i].name);
 		}
 	}
 }
