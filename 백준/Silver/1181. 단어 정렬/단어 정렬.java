4f7dadc6134253de9b6825a5090c5b170d6e6b61
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-02-23 17:53:55 +0900

    [Silver V] Title: 단어 정렬, Time: 420 ms, Memory: 22468 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,26 +3,25 @@ import java.util.*;
 
 public class Main {
 	static int N;
-	static String[] arr;
+	static String[] str1;
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new String[N];
+		str1 = new String[N];
 		for (int i = 0; i < N; i++) {
-			String s = br.readLine();
-			arr[i] = s;
+			str1[i] = br.readLine();
 		}
-		Arrays.sort(arr, (o1, o2) -> {
+		Arrays.sort(str1, (o1, o2) -> {
 			if (o1.length() == o2.length()) {
 				return o1.compareTo(o2);
 			}
 			return o1.length() - o2.length();
 		});
-		System.out.println(arr[0]);
-		for (int i = 1; i < N; i++) {
-			if (arr[i].equals(arr[i - 1]) == false) {
-				System.out.println(arr[i]);
+		System.out.println(str1[0]);
+		for (int i = 1; i < str1.length; i++) {
+			if (str1[i].equals(str1[i - 1]) == false) {
+				System.out.println(str1[i]);
 			}
 		}
 	}
