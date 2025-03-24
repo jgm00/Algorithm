607c45e6c7da284c0622fa9b1fe67e29be5711d3
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-03-24 11:01:44 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,13 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static int[][] board;
-	static int[][] dist;
-
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-
 	static class Pair {
 		int x;
 		int y;
@@ -19,43 +12,51 @@ public class Main {
 		}
 	}
 
+	static int board[][];
+	static int visited[][];
+	static int N, M;
+
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+
 	static Queue<Pair> q = new LinkedList<>();
+
+	static void bfs() {
+		int curx = 0;
+		int cury = 0;
+		visited[curx][cury] = 1;
+		q.add(new Pair(curx, cury));
+		while (!q.isEmpty()) {
+			Pair p = q.poll();
+			curx = p.x;
+			cury = p.y;
+			for (int dir = 0; dir < 4; dir++) {
+				int nx = dx[dir] + curx;
+				int ny = dy[dir] + cury;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+					continue;
+				if (board[nx][ny] == 0 || visited[nx][ny] != 0)
+					continue;
+				visited[nx][ny] = visited[curx][cury] + 1;
+				q.add(new Pair(nx, ny));
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		board = new int[N][M];
-		dist = new int[N][M];
+		visited = new int[N][M];
 		for (int i = 0; i < N; i++) {
-			Arrays.fill(dist[i], -1);
-			String s = br.readLine();
+			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				int k = s.charAt(j) - '0';
-				board[i][j] = k;
+				board[i][j] = str.charAt(j) - '0';
 			}
 		}
-		dist[0][0] = 0;
-		q.add(new Pair(0, 0));
-
-		while (!q.isEmpty()) {
-			Pair cur = q.poll();
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur.x + dx[dir];
-				int ny = cur.y + dy[dir];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-					continue;
-				if (dist[nx][ny] != -1 || board[nx][ny] != 1)
-					continue;
-
-				dist[nx][ny] = dist[cur.x][cur.y] + 1;
-				q.add(new Pair(nx, ny));
-			}
-
-		}
-		System.out.println(dist[N-1][M-1] + 1);
+		bfs();
+		System.out.println(visited[N - 1][M - 1]);
 	}
 }
