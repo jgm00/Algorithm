93eae94eb08c4ddfe7a68e8c15e56c182ebf7a3a
mJegal <132114851+jgm00@users.noreply.github.com>
2025-07-22 03:32:00 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 119384 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,43 +1,47 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main
-{
-    static int N,M;
-    static int[] arr;
-    static boolean isT(int k){
-        Long sum =0L;
-        for(int i=0;i<N;i++){
-            sum += Math.max((arr[i] - k),0);
-        }
-        if(M > sum){
-            return false;
-        }
-        return true;
-    }
+public class Main {
+	static int[] tree;
+	static int N, M;
+
+	static boolean check(int k) {
+		long sum = 0;
+		for (int i = 0; i < tree.length; i++) {
+			if (tree[i] >= k) {
+				sum += (tree[i] - k);
+			}
+		}
+		if (sum >= M) {
+			return true;
+		}
+		return false;
+	}
+
+	static int binary_lower(int l, int r) {
+		int ret = 0;
+		while (l <= r) {
+			int mid = (int) ((l + r) / 2);
+			if (check(mid) == true) {
+				ret = mid;
+				l = mid + 1;
+			} else {
+				r = mid - 1;
+			}
+		}
+		return ret;
+	}
+
 	public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
-       N = Integer.parseInt(st.nextToken());
-       M = Integer.parseInt(st.nextToken());
-       arr = new int[N];
-       st = new StringTokenizer(br.readLine());
-       for(int i=0;i<N;i++){
-           arr[i] = Integer.parseInt(st.nextToken());
-       }
-        Arrays.sort(arr);
-       int l = 0; int r = 2000000000;
-       int ans = 0;
-       while(l<=r){
-           int mid = (l+r) / 2;
-           if(isT(mid) == false){
-               r = mid -1; 
-           }else
-           {
-               l = mid + 1;
-               ans = mid;
-           }
-       }
-       System.out.println(ans);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		tree = new int[N];
+		for (int i = 0; i < N; i++) {
+			tree[i] = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(binary_lower(0, 2000000000));
 	}
 }
