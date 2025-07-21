53911a7f52a559aaf24645f011df79c40ede9d68
mJegal <132114851+jgm00@users.noreply.github.com>
2025-07-21 23:14:57 +0900

    [Gold V] Title: 두 용액, Time: 360 ms, Memory: 28584 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,60 +1,55 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-	static int N;
-	static int[] A;
-	static int mn = Integer.MAX_VALUE;
-
-	static int bin_search(int L, int R, int tar) {
-		int res = L - 1;
-		while (L <= R) {
-			int mid = (L + R) / 2;
-			if (A[mid] < tar) {
-				res = mid;
-				L = mid + 1;
-			} else {
-				R = mid - 1;
-			}
-		}
-		return res;
-	}
-
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		A = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(A);
-		int ans1 = 0;
-		int ans2 = 0;
-		for (int i = 0; i < N; i++) {
-			int k = bin_search(0, N - 1, -A[i]);
-			if (0 <= k && k < N && A[k] != A[i]) {
-				int abs1 = Math.abs(A[k] + A[i]);
-				if (mn > abs1) {
-					mn = abs1;
-					ans1 = A[k];
-					ans2 = A[i];
-				}
-			}
-			if (-1 <= k && k < N-1 && A[k+1] != A[i]) {
-				int abs1 = Math.abs(A[k+1] + A[i]);
-				if (mn > abs1) {
-					mn = abs1;
-					ans1 = A[k+1];
-					ans2 = A[i];
-				}
-			}
-		}
-		if (ans1 < ans2) {
-			System.out.println(ans1 + " " + ans2);
-		} else {
-			System.out.println(ans2 + " " + ans1);
-		}
+public class Main
+{
+    static int N;
+    static int[] arr;
+    static int bin_s(int tar){
+        int l = 0; int r = N-1;
+        int idx = 0;
+        while(l<=r){
+            int mid = (r+l) / 2;
+            if(arr[mid] < tar){
+                l = mid + 1;
+                idx = l;
+            }else if(arr[mid] > tar){
+                r = mid - 1;
+            }else {
+                return mid; 
+            }
+        }
+        return idx;
+    }
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i =0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int mn = Integer.MAX_VALUE;
+        int a = 0; int b = 0;
+        for(int i=0;i<N;i++){
+            int k = bin_s(-1 * arr[i]);
+            if(N > k && k > -1){
+            if(arr[k] != arr[i]) {
+                if(mn > Math.abs(arr[i] + arr[k])){
+                    mn = Math.abs(arr[i] + arr[k]);
+                    a = Math.min(arr[i], arr[k]);
+                    b = Math.max(arr[i], arr[k]);
+                }}}
+            if(k-1>=0){
+            if(arr[k-1] != arr[i]){
+                if(mn > Math.abs(arr[i] + arr[k-1])){
+                    mn = Math.abs(arr[i] + arr[k-1]);
+                    a = Math.min(arr[i], arr[k-1]);
+                    b = Math.max(arr[i], arr[k-1]);
+                }
+            }}
+        }
+        System.out.println(a + " " + b);
 	}
 }
