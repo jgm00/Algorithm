159d71f7a219f11608ca6d238b2dbb0cda1a27a7
mJegal <132114851+jgm00@users.noreply.github.com>
2025-07-21 18:22:41 +0900

    [Silver IV] Title: 수 찾기, Time: 1140 ms, Memory: 53100 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	static int N, M;
-	static int[] A, B;
-
-	static int binary_search(int L, int R, int target) {
-		while (L <= R) {
-			int mid = (R + L) / 2;
-			if (A[mid] < target) {
-				L = mid + 1;
-			} else if (A[mid] > target) {
-				R = mid - 1;
-			} else {
-				return 1;
-			}
-		}
-		return 0;
-	}
-
-	public static void main(String args[]) throws IOException {
+public class Main
+{
+    static int N,M;
+    static int[] A;
+    static int binary_search(int k, int l, int r){
+        while(l<=r){
+            int mid = (l+r) / 2;
+            if(A[mid] < k){
+                l = mid + 1;
+            }else if(A[mid] > k){
+                r = mid - 1;
+            }else{
+                return 1;
+            }
+        }
+        return 0;
+    }
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		A = new int[N];
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++){
+		    A[i] = Integer.parseInt(st.nextToken());
 		}
 		M = Integer.parseInt(br.readLine());
-		B = new int[M];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			B[i] = Integer.parseInt(st.nextToken());
-		}
 		Arrays.sort(A);
-		for (int i = 0; i < M; i++) {
-			System.out.println(binary_search(0,N-1,B[i]));
+		for(int i=0;i<M;i++){
+		    int k = Integer.parseInt(st.nextToken());
+		    System.out.println(binary_search(k,0,N-1));
 		}
 	}
 }
