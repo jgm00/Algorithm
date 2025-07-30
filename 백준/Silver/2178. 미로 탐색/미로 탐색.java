553293cfe35e46ceac73da9c40e2a71d1436f762
mJegal <132114851+jgm00@users.noreply.github.com>
2025-07-30 15:42:51 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,62 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static class Pair {
-		int x;
-		int y;
-
-		Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	static int board[][];
-	static int visited[][];
-	static int N, M;
-
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-
-	static Queue<Pair> q = new LinkedList<>();
-
-	static void bfs() {
-		int curx = 0;
-		int cury = 0;
-		visited[curx][cury] = 1;
-		q.add(new Pair(curx, cury));
-		while (!q.isEmpty()) {
-			Pair p = q.poll();
-			curx = p.x;
-			cury = p.y;
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = dx[dir] + curx;
-				int ny = dy[dir] + cury;
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-					continue;
-				if (board[nx][ny] == 0 || visited[nx][ny] != 0)
-					continue;
-				visited[nx][ny] = visited[curx][cury] + 1;
-				q.add(new Pair(nx, ny));
-			}
-		}
-	}
-
+public class Main
+{
+    static int N,M;
+    static int[][] board;
+    static int[][] vis;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
+    static Queue<Integer> q = new LinkedList<>();
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		board = new int[N][M];
-		visited = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				board[i][j] = str.charAt(j) - '0';
-			}
-		}
-		bfs();
-		System.out.println(visited[N - 1][M - 1]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        vis = new int[N][M];
+        for(int i=0;i<N;i++){
+            String s = br.readLine();
+            for(int j=0;j<M;j++){
+                board[i][j] = s.charAt(j) - '0';
+            }
+        }
+        vis[0][0] = 1;
+        q.add(0); q.add(0);
+        while(!q.isEmpty()){
+            int curx = q.poll();
+            int cury = q.poll();
+            for(int dir =0;dir<4;dir++){
+                int nx = curx + dx[dir];
+                int ny = cury + dy[dir];
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(board[nx][ny] == 0 || vis[nx][ny] != 0) continue;
+                q.add(nx); q.add(ny);
+                vis[nx][ny] = vis[curx][cury] + 1;
+            }
+        }
+        System.out.println(vis[N-1][M-1]);
+        
 	}
 }
