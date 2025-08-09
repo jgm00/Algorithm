2529602b0601eb33ab444a3ff8fd0113a7438bea
mJegal <132114851+jgm00@users.noreply.github.com>
2025-08-10 01:54:00 +0900

    [Gold III] Title: 파티, Time: 172 ms, Memory: 17360 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,83 +1,71 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int N, M, X;
-    static class Pair {
-        int x, y;
-        Pair(int x, int y) {
+public class Main
+{
+    static int N,M,X;
+    static class Pair{
+        int x,y;
+        Pair(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-
-    static ArrayList<Pair>[] adj;
+    static int mn = Integer.MIN_VALUE;
+    static ArrayList<Pair>[] adj,revadj;
     static int[] dist;
     static PriorityQueue<Pair> pq;
-
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-
-        adj = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
+        adj = new ArrayList[N+1];
+        revadj = new ArrayList[N+1];
+        for(int i=1;i<=N;i++){
             adj[i] = new ArrayList<>();
+            revadj[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < M; i++) {
+        for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
-            adj[a].add(new Pair(b, d));
+            adj[a].add(new Pair(b,d));
+            revadj[b].add(new Pair(a,d));
         }
-
-        int[] distFromX = new int[N + 1];
-        Arrays.fill(distFromX, Integer.MAX_VALUE);
-        distFromX[X] = 0;
-        pq = new PriorityQueue<>((o1, o2) -> o1.y - o2.y);
-        pq.add(new Pair(X, 0));
-
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            if (p.y != distFromX[p.x]) continue;
-
-            for (Pair e : adj[p.x]) {
-                if (distFromX[p.x] + e.y < distFromX[e.x]) {
-                    distFromX[e.x] = distFromX[p.x] + e.y;
-                    pq.add(new Pair(e.x, distFromX[e.x]));
-                }
-            }
-        }
-
-        int maxDistance = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            if (i == X) continue; 
-
-            dist = new int[N + 1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[i] = 0;
-            pq.clear();
-            pq.add(new Pair(i, 0));
-
-            while (!pq.isEmpty()) {
+        int[] ans = new int[N+1];
+            pq = new PriorityQueue<>((o1,o2)->{return o1.y-o2.y;});
+            Arrays.fill(ans, Integer.MAX_VALUE);
+            ans[X] = 0;
+            pq.add(new Pair(X,0));
+            while(!pq.isEmpty()){
                 Pair p = pq.poll();
-                if (p.y != dist[p.x]) continue;
-
-                for (Pair e : adj[p.x]) {
-                    if (dist[p.x] + e.y < dist[e.x]) {
-                        dist[e.x] = dist[p.x] + e.y;
-                        pq.add(new Pair(e.x, dist[e.x]));
-                    }
+                if(p.y != ans[p.x]) continue;
+                for(Pair e : revadj[p.x]){
+                    if((ans[p.x] + e.y) >= ans[e.x]) continue;
+                    ans[e.x] = ans[p.x] + e.y;
+                    pq.add(new Pair(e.x,ans[e.x]));
                 }
             }
-
-            maxDistance = Math.max(maxDistance, dist[X] + distFromX[i]);
+            pq = new PriorityQueue<>((o1,o2)->{return o1.y-o2.y;});
+            dist = new int[N+1];
+            Arrays.fill(dist, Integer.MAX_VALUE);        
+            dist[X] = 0;
+            pq.add(new Pair(X,0));
+            while(!pq.isEmpty()){
+                Pair p = pq.poll();
+                if(p.y != dist[p.x]) continue;
+                for(Pair e : adj[p.x]){
+                    if((dist[p.x] + e.y) >= dist[e.x]) continue;
+                    dist[e.x] = dist[p.x] + e.y;
+                    pq.add(new Pair(e.x,dist[e.x]));
+                }
+            }
+        for(int i=1;i<=N;i++){
+            mn = Math.max(mn, (dist[i]+ans[i]) );
         }
-
-        System.out.println(maxDistance);
-    }
+        System.out.println(mn);
+	}
 }
