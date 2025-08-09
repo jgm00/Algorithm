3a875aceb1a079148767f141b4a771e9115a1728
mJegal <132114851+jgm00@users.noreply.github.com>
2025-08-09 22:21:04 +0900

    [Gold V] Title: 집합의 표현, Time: 964 ms, Memory: 52124 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,M;
+    static int[] p;
+    static int[] r;
+    static int find(int x){
+        if(p[x] == -1) return x;
+        return p[x] = find(p[x]);
+    }
+    static void merge(int a, int b){
+        a = find(a); 
+        b = find(b);
+        if(a==b) return;
+        p[b] = a;
+    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		p = new int[N+1];
+		Arrays.fill(p,-1);
+		for(int i=1;i<=M;i++){
+		    st = new StringTokenizer(br.readLine());
+		    int a = Integer.parseInt(st.nextToken());
+		    int b = Integer.parseInt(st.nextToken());
+		    int c = Integer.parseInt(st.nextToken());
+		    if(a == 0){
+		        merge(b,c);
+		    }else{
+		        if(find(b)==find(c)){
+		            System.out.println("YES");
+		        }else{
+		            System.out.println("NO");
+		        }
+		        
+		    }
+		}
+	}
+}
