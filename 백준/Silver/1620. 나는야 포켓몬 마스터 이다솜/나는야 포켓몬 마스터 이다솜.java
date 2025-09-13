eba76bcbf7490890c87c51dbae3ee9ecf35d9140
mJegal <132114851+jgm00@users.noreply.github.com>
2025-09-13 23:34:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 912 ms, Memory: 54572 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,28 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-	static HashMap<Integer, String> hmap1 = new HashMap<>();
-	static HashMap<String, Integer> hmap2 = new HashMap<>();
-
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < N+1; i++) {
-			String name = br.readLine();
-			hmap1.put(i, name);
-			hmap2.put(name, i);
-		}
-		for (int i = 0; i < M; i++) {
-			String name = br.readLine();
-			if(Character.isDigit(name.charAt(0))) {
-				System.out.println(hmap1.get(Integer.parseInt(name)));
-			} else {
-				System.out.println(hmap2.get(name));
-			}
-		}
+public class Main
+{
+    static int N,M;
+    static HashMap<String, Integer> hmap = new HashMap<>();
+    static HashMap<Integer, String> hmap2 = new HashMap<>();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;i++){
+            String s = br.readLine();
+            hmap.put(s,(i+1));
+            hmap2.put((i+1),s);
+        }
+        for(int i=0;i<M;i++){
+            String s = br.readLine();
+            char c = s.charAt(0);
+            if('0' <= c && c <= '9'){
+                int k = Integer.parseInt(s);
+                System.out.println(hmap2.get(k));
+            }else{
+                System.out.println(hmap.get(s));
+            }
+        }
 	}
 }
