01bff96c00d87d789e4bb5f4a41d31dfd80f9d36
mJegal <132114851+jgm00@users.noreply.github.com>
2025-09-19 15:52:54 +0900

    [level 2] Title: 카펫, Time: 7.79 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sm = brown+yellow;
-        for(int i=yellow;i>0; i--){
-            if(yellow%i==0){
-                if(sm ==(i+2) * (yellow/i+2)){
-                    System.out.println(i);
-                    answer[1] = (yellow/i+2);
-                    answer[0] = (i+2);
-                    break;
-                }
-            }
-            
-        }
+        int[] answer = {};
         return answer;
     }
 }
