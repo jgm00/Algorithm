1a6cfdf3e445e4697c0a61df134e21401dc59ae9
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-04 04:28:46 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{   static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+	    sb.append("\\    /\\").append("\n");
+	    sb.append(" )  ( ')").append("\n");
+	    sb.append("(  /  )").append("\n");
+	    sb.append(" \\(__)|").append("\n");
+	    System.out.println(sb.toString());
+	}
+}
