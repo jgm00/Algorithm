cbef4c034954b935c94179f2754a3f8f6de2c990
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-04 04:31:08 +0900

    [Bronze V] Title: 개, Time: 92 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{   static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+	    sb.append("|\\_/|").append("\n");
+	    sb.append("|q p|   /}").append("\n");
+	    sb.append("( 0 )\"\"\"\\").append("\n");
+	    sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|").append("\n");
+	    System.out.println(sb.toString());
+	}
+}
