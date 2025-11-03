ffa1728df4f1faba02bf7e0b7c3800d9fa9d4732
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-04 04:08:28 +0900

    [Bronze V] Title: A/B, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{   static int N,M;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        System.out.println((double) N / M);
+	}
+}
