1924422805da01f7cb94284174a5977cfcfb0b31
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-04 04:12:04 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{   static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb.append(br.readLine()).append("??!");
+        System.out.println(sb.toString());
+	}
+}
