1ba51b6bbb90025c946ab31115e99fb5784eed88
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-11 18:49:50 +0900

    [Silver IV] Title: 문자열 집합, Time: 356 ms, Memory: 30956 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,40 +1,23 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main
-{
-    static int N,M;
-    static int cnt = 0;
-    static String[] arr;
-    static boolean bin_search(String s, int l, int r){
-        while(l <= r){
-            int mid = (l + r) / 2;
-            if(s.equals(arr[mid])){
-                return true;
-            }else if(arr[mid].compareTo(s) < 0){
-                l = mid + 1;
-            }else if(arr[mid].compareTo(s) > 0){
-                r = mid - 1;
-            }
-        }
-        return false;
-    }
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    static int N,M, ans;
+    static HashSet<String> hset = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new String[N];
         for(int i=0;i<N;i++){
-            arr[i] = br.readLine();
+            hset.add(br.readLine());
         }
-        Arrays.sort(arr);
-        for(int i =0;i<M;i++){
+        for(int i=0;i<M;i++){
             String s = br.readLine();
-            if(bin_search(s,0,N-1)){
-                cnt += 1;
+            if(hset.contains(s)){
+                ans += 1;
             }
         }
-        System.out.println(cnt);
-	}
+        System.out.println(ans);
+    }
 }
