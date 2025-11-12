5b1467c61e0439590f26f21e6d117edc4774c9f4
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-12 18:18:22 +0900

    [level 1] Title: 체육복, Time: 0.50 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int ans = n;
-        Arrays.sort(lost); Arrays.sort(reserve);
-        for(int i=0;i<reserve.length;i++){
-            for(int j=0;j<lost.length;j++){
-                if(reserve[i] == lost[j]){
-                    lost[j]=-1;
-                    reserve[i] = -1;
-                    break;
-                }
-            }
-        }      
-        for(int i=0;i<reserve.length;i++){
-            if(reserve[i] == -1) continue;
-            for(int j=0;j<lost.length;j++){
-                if(lost[j] == -1) continue;
-                if(reserve[i] - 1 == lost[j] || reserve[i] + 1 == lost[j]){
-                    lost[j] = -1;
-                    break;
-                }
-            }
-        }
-        for(int k : lost){
-            if(k!=-1){
-                answer += 1;
-            }
-        }
-        return n - answer;
+        return answer;
     }
 }
