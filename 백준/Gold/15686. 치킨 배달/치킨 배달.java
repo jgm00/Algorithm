d439928f6ce41a6fd1d7400440dd7cd50b565de4
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-18 17:47:52 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,79 +1,76 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M;
-	static int[][] board;
-	static int[] vis;
-
-	static class Pair {
-		int x, y;
-
-		Pair(int x, int y) {
+	static int N,M;
+	static class Pair{
+		int x,y;
+		Pair(int x, int y){
 			this.x = x;
 			this.y = y;
 		}
 	}
-
+	static int sz;
+	static int[][] board;
+	static ArrayList<Pair> home = new ArrayList<>();
 	static ArrayList<Pair> chicken = new ArrayList<>();
-	static ArrayList<Pair> house = new ArrayList<>();
 	static int[] arr;
-	static int MN = Integer.MAX_VALUE;
-
-	static void dfs(int k) {
-		if (k == M) {
-			int sum = 0;
-			for (int d = 0; d < house.size(); d++) {
-				int mn = Integer.MAX_VALUE;
-				for (int i = 0; i < M; i++) {
-					Pair h = house.get(d);
-					Pair c = chicken.get(arr[i]);
-					mn = Math.min(mn, (Math.abs(h.x - c.x) + Math.abs(h.y - c.y)));
-				}
-				sum += mn;
-				if(sum > MN) {
-					return;
-				}
+	static int[] vis;
+	static int mn = Integer.MAX_VALUE;
+	static int ans = Integer.MAX_VALUE;
+	static void cal(int[] arr) {
+		int sm = 0;
+		for(int i=0;i<home.size();i++) {
+			Pair h = home.get(i);
+			mn = Integer.MAX_VALUE;
+			for(int j=0;j<arr.length;j++) {
+				Pair p = chicken.get(arr[j]);
+				int dffx = Math.abs(h.x-p.x);
+				int dffy = Math.abs(h.y-p.y);
+				mn = Math.min(mn, (dffx+dffy));
 			}
-			MN = Math.min(MN, sum);
-			sum = 0;
+			sm += mn;
+		}
+		ans = Math.min(ans, sm);
+	}
+	static void dfs(int k) {
+		if(k==M) {
+			cal(arr);
 			return;
 		}
-		int st = 0;
-		if (k != 0) {
-			st = arr[k - 1];
+		int start = 0;
+		if(k!=0) {
+			start = arr[k-1];
 		}
-		for (int i = st; i < chicken.size(); i++) {
-			if (vis[i] == 1)
-				continue;
-			arr[k] = i;
-			vis[i] = 1;
-			dfs(k + 1);
-			vis[i] = 0;
+		for(int i=start;i<sz;i++) {
+			if(vis[i] == 0) {
+				arr[k] = i;
+				vis[i] = 1;
+				dfs(k+1);
+				vis[i] = 0;
+			}
 		}
 	}
-
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		board = new int[N][N];
-		for (int i = 0; i < N; i++) {
+		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+			for(int j=0;j<N;j++) {
 				int k = Integer.parseInt(st.nextToken());
-				if (k == 2) {
-					chicken.add(new Pair(i, j));
-				} else if (k == 1) {
-					house.add(new Pair(i, j));
+				if(k == 1) {
+					home.add(new Pair(i,j));
+				}else if(k==2) {
+					chicken.add(new Pair(i,j));
 				}
 			}
 		}
-		int sz = chicken.size();
+		sz = chicken.size();
 		arr = new int[M];
-		vis = new int[sz];
+		vis = new int[sz+1];
 		dfs(0);
-		System.out.println(MN);
+		System.out.println(ans);
 	}
 }
