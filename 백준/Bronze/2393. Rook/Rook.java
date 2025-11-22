426704bfec82d2bcb077f5c0c7ae5054d32c03b8
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-23 04:50:21 +0900

    [Bronze V] Title: Rook, Time: 92 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int T;
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("  ___  ___  ___");
+		System.out.println("  | |__| |__| |");
+		System.out.println("  |           |");
+		System.out.println("   \\_________/");
+		System.out.println("    \\_______/");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |     |");
+		System.out.println("     |_____|");
+		System.out.println("  __/       \\__");
+		System.out.println(" /             \\");
+		System.out.println("/_______________\\");
+
+	}
+}
