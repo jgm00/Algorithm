2583b1f1c99b6cf1b06ade518c07976641bd1857
mJegal <132114851+jgm00@users.noreply.github.com>
2025-11-23 19:08:07 +0900

    [Bronze V] Title: 기찍 N, Time: 700 ms, Memory: 30000 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static int[][] board;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		for (int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
