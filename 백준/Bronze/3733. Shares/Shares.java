ef53d5edb05543a16c50b38d87c5def3eeff0542
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 17:48:38 +0900

    [Bronze V] Title: Shares, Time: 172 ms, Memory: 17232 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    System.out.println(b/(a+1));
+		}
+		
+		
+	}
+}
