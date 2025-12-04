063ada00574125dd9beaade5992a8eba9ef0b615
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:52:20 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 96 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println("비와이");
+	}
+}
