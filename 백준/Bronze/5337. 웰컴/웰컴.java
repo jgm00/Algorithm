1186c0dd40e8b2c2b8fca573b4e9004e87738fa4
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:43:06 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println(".  .   .");
+    	System.out.println("|  | _ | _. _ ._ _  _");
+    	System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
