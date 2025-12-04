8239a8543ad06fdb30d4bd8186740788500f43cb
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:53:49 +0900

    [Bronze V] Title: 나는 누구인가, Time: 96 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+    	System.out.println("I'm Sexy");
+	}
+}
