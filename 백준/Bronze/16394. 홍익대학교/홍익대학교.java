5d7fb6291b3eb13f0c731fa4d1da8ddcf036dd3e
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 19:23:16 +0900

    [Bronze V] Title: 홍익대학교, Time: 180 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		System.out.println(N - 1946);
+
+	}
+}
