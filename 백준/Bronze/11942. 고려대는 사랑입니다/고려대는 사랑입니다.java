a92be5d1c81b45fe1bd2474be5755df4ee95ca62
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:28:02 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 108 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println("고려대학교");
+	}
+}
