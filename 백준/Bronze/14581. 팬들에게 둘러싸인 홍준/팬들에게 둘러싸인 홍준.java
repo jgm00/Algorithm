df5a24bb9f5b5038e172217ae5a2f1782903e714
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:19:17 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 120 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(":fan::fan::fan:");
+	    System.out.print(":fan:");
+	    System.out.print(":"+s+":");
+	    System.out.println(":fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
