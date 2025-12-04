bdf95cf6a77eface41edde25e565455d74437b97
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 18:36:30 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println("       _.-;;-._");
+    	System.out.println("'-..-'|   ||   |");
+    	System.out.println("'-..-'|_.-;;-._|");
+    	System.out.println("'-..-'|   ||   |");
+    	System.out.println("'-..-'|_.-''-._|");
+	}
+}
