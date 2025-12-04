af5ca5bc42df15b5f7e3eb8cf128a5cc9b44da1f
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-04 19:24:27 +0900

    [Bronze V] Title: 연세대학교, Time: 184 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,S;
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		if(N==0){
+		    System.out.println("YONSEI");
+		}else if(N==1){
+		    System.out.println("Leading the Way to the Future");
+		}
+
+	}
+}
