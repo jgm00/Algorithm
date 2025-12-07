3d42c7813ac890dcd5f86c871bb755cbe1225650
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-07 17:57:54 +0900

    [Platinum V] Title: LCA 2, Time: 1376 ms, Memory: 109268 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.java
+++ 백준/Platinum/11438. LCA 2/LCA 2.java
@@ -1,81 +1,76 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-	static int N;
-	static ArrayList<Integer>[] graph;
-	static int M;
-	static int[] d;
-	static int[][] par;
-
-	static int LCA(int a, int b) {
-		if (d[a] > d[b]) {
-			int temp = a;
-			a = b;
-			b = temp;
-		}
-
-		for (int i = 19; i >= 0; i--) {
-			if (d[b] - d[a] >= (1 << i)) {
-				b = par[b][i];
-			}
-		}
-
-		if (a == b)
-			return a;
-
-		for (int i = 19; i >= 0; i--) {
-			if (par[a][i] != par[b][i]) {
-				a = par[a][i];
-				b = par[b][i];
-			}
-		}
-		return par[a][0];
-	}
-
-	static StringTokenizer st;
-
-	static void dfs(int cur) {
-		for (int nxt : graph[cur]) {
-			if (nxt == par[cur][0])
-				continue;
-			par[nxt][0] = cur;
-			d[nxt] = d[cur] + 1;
-			dfs(nxt);
-		}
-	}
-
+public class Main
+{   
+    static int N,M;
+    static int mxL;
+    static int[][] parent;
+    static int[] depth;
+    static ArrayList<Integer>[] adj;
+    static int LCA(int a, int b){
+        if(depth[a]>depth[b]){
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        for(int i=mxL;i>=0;i--){
+            if(depth[b] - depth[a] >= (1<<i)){
+                b = parent[b][i];
+            }
+        }
+        if(a == b){
+            return a;
+        }
+        for(int i=mxL; i>=0;i--){
+            if(parent[a][i] != parent[b][i]) {
+                a = parent[a][i];
+                b = parent[b][i];
+            }
+        }
+        return parent[a][0];
+    }
+    
+    static void dfs(int k){
+        for(int nxt : adj[k]){
+            if(parent[k][0] == nxt) continue;
+            parent[nxt][0] = k;
+            depth[nxt] = depth[k] + 1;
+            dfs(nxt);
+        }
+    }
+    
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		graph = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		par = new int[N + 1][20];
-		d = new int[N + 1];
-		for (int i = 0; i < N - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int v1 = Integer.parseInt(st.nextToken());
-			int v2 = Integer.parseInt(st.nextToken());
-			graph[v1].add(v2);
-			graph[v2].add(v1);
-		}
-		par[1][0] = 0;
-		d[1] = 0;
-		dfs(1);
-		for (int i = 1; i < 20; i++) {
-			for (int j = 1; j <= N; j++) {
-				par[j][i] = par[par[j][i - 1]][i - 1];
-			}
-		}
-		M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int v1 = Integer.parseInt(st.nextToken());
-			int v2 = Integer.parseInt(st.nextToken());
-			System.out.println(LCA(v1, v2));
-		}
+        N = Integer.parseInt(br.readLine());
+        mxL = (int) Math.floor(Math.log10(N) / Math.log10(2));
+        adj = new ArrayList[N+1];
+        parent = new int[N+1][mxL+1];
+        depth = new int[N+1];
+        for(int i=1;i<=N;i++){
+            adj[i] = new ArrayList<>();
+        }
+        for(int i=0;i<N-1;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            adj[v].add(e);
+            adj[e].add(v);
+        }
+        parent[1][0] = 0;
+        depth[1]= 0;
+        dfs(1);
+        for(int i=1;i<=mxL;i++){
+            for(int j=1;j<=N;j++){
+                parent[j][i] = parent[parent[j][i-1]][i-1];
+            }
+        }
+        M = Integer.parseInt(br.readLine());
+        for(int i=0;i<M;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            System.out.println(LCA(v,e));
+        }
 	}
 }
