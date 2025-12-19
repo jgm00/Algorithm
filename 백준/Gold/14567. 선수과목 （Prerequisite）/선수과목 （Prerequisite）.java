66a4fd50a40e910e68336a0336f5b55a5c3112c7
mJegal <132114851+jgm00@users.noreply.github.com>
2025-12-19 23:14:23 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 596 ms, Memory: 129804 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N,M,a,b;
+    static ArrayList<Integer>[] adj;
+    static int[] dist, deg, ans;
+    static PriorityQueue<Integer> pq  = new PriorityQueue<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		adj = new ArrayList[N+1];
+		deg = new int[N+1];
+		ans = new int[N+1];
+		for(int i=1;i<=N;i++){
+		    adj[i] = new ArrayList<>();
+		}
+		for(int i = 0; i<M;i++){
+		    st = new StringTokenizer(br.readLine());
+        	a = Integer.parseInt(st.nextToken());
+    		b = Integer.parseInt(st.nextToken());
+    		adj[a].add(b);
+    		deg[b] += 1;
+		}
+		for(int i=1;i<=N;i++){
+		    if(deg[i] == 0) pq.add(i);
+		}
+		while(!pq.isEmpty()){
+		    int cur = pq.poll();
+		    for(int k : adj[cur]){
+		        ans[k] = Math.max(ans[cur]+1,ans[k]);
+		        deg[k] -=1;
+		        if(deg[k] == 0) pq.add(k);
+		    }
+		}
+		for(int i=1;i<=N;i++){
+		    System.out.print((ans[i]+1) + " ");
+		}
+	}
+}
